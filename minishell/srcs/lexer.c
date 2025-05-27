0d1e674ff71abbe0026e5c7e66836e05f1b7e82c
Bozil <161837532+BastienOzil@users.noreply.github.com>
2025-05-27 15:57:29 +0200

    Lexer a jour 27/05

--- minishell/srcs/lexer.c
+++ minishell/srcs/lexer.c
@@ -1,3 +1,84 @@
 #include "../includes/minishell.h"
 
-//séparation des actions et prise en compte de $ et '' et ""
+char	*ft_substr(char *s, int start, int len)
+{
+	char	*substr;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	substr = malloc(len + 1);
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+void	skip_spaces(t_lexer *lexer)
+{
+	while (lexer->input[lexer->i] && is_space(lexer->input[lexer->i]))
+		lexer->i++;
+}
+
+char	*get_word(t_lexer *lexer)
+{
+	int	start;
+
+	start = lexer->i;
+	while (lexer->input[lexer->i] && !is_space(lexer->input[lexer->i]) 
+		&& !is_special(lexer->input[lexer->i]))
+		lexer->i++;
+	return (ft_substr(lexer->input, start, lexer->i - start));
+}
+
+t_token	*new_token(t_token_type type, char *value)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = value;
+	token->next = NULL;
+	return (token);
+}
+
+t_token	*get_next_token(t_lexer *lexer)
+{
+	skip_spaces(lexer);
+	if (!lexer->input[lexer->i])
+		return (new_token(TOKEN_EOF, NULL));
+	if (lexer->input[lexer->i] == '|')
+	{
+		lexer->i++;
+		return (new_token(TOKEN_PIPE, ft_substr("|", 0, 1)));
+	}
+	if (lexer->input[lexer->i] == '<')
+	{
+		if (lexer->input[lexer->i + 1] == '<')
+		{
+			lexer->i += 2;
+			return (new_token(TOKEN_HEREDOC, ft_substr("<<", 0, 2)));
+		}
+		lexer->i++;
+		return (new_token(TOKEN_REDIR_IN, ft_substr("<", 0, 1)));
+	}
+	if (lexer->input[lexer->i] == '>')
+	{
+		if (lexer->input[lexer->i + 1] == '>')
+		{
+			lexer->i += 2;
+			return (new_token(TOKEN_APPEND, ft_substr(">>", 0, 2)));
+		}
+		lexer->i++;
+		return (new_token(TOKEN_REDIR_OUT, ft_substr(">", 0, 1)));
+	}
+	return (new_token(TOKEN_WORD, get_word(lexer)));
+}
