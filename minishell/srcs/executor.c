e445682fe37f09c39288eed1f60b0e0fa0ab0edd
relia31 <aurgeorg@c2r4p1.42nice.fr>
2025-06-02 17:03:07 +0200

    ajout commentaires et fork gestion d erreur

--- minishell/srcs/executor.c
+++ minishell/srcs/executor.c
@@ -1,11 +1,21 @@
 #include "../includes/minishell.h"
 
 //execution des commandes (fork, execve, redirections, pipes, dup2, close)
+// Elle exécute une commande (comme ls, echo, etc.) en créant un nouveau processus 
+// à l’aide de fork() et en lançant la commande avec execve()
 void	execute_cmd(t_cmd *cmd, char **envp)
 {
 	pid_t	pid;
 
+	//cree un processus enfant
 	pid = fork();
+	//si fork echoue
+	if (pid < 0)
+	{
+		perror("fork");
+		return ;
+	}
+	//cree processus enfant
 	if (pid == 0)
 	{
 		execve(cmd->argv[0], cmd->argv, envp);
