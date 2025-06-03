f6f80e2640c8c4e103c01940ddee398ae1315caf
Bozil <161837532+BastienOzil@users.noreply.github.com>
2025-06-03 18:17:10 +0200

    Delete minishell directory

--- minishell/srcs/builtins.c
+++
@@ -1,3 +0,0 @@
-#include "../includes/minishell.h"
-
-//detections des apelle de fonctions interne (echo, cd, pwd, env, export, unset, exit)
--- minishell/srcs/input.c
+++
@@ -1,3 +0,0 @@
-#include "../includes/minishell.h"
-
-//gestion et utilisation de readline, add_history() et l'invite
