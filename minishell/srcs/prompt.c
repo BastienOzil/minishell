20df90e0f61413dbb7611bd5eb2023b88fa2a1da
BastienOzil <bastien.ozil17@gmail.com>
2025-06-13 21:07:52 +0200

    merge in progress

---
+++ minishell/srcs/prompt.c
@@ -0,0 +1,64 @@
+#include "../includes/minishell.h"
+
+// boucle de token
+void	handle_tokens(t_lexer *lexer)
+{
+	t_token	*token;
+
+	while (1)
+	{
+		token = get_next_token(lexer);
+		print_token(token);
+		if (token->type == TOKEN_EOF)
+		{
+			free_tokens(token);
+			break ;
+		}
+		free_tokens(token);
+	}
+}
+
+// traitement de la ligne
+void	handle_line(char *line)
+{
+	t_lexer	lexer;
+
+	lexer.input = line;
+	lexer.i = 0;
+	handle_tokens(&lexer);
+}
+
+// // ancien main de bastien
+// void	ft_run_interactive(char **envp)
+// {
+// 	char	*line;
+
+// 	while (1)
+// 	{
+// 		line = readline("\001\033[1;36m\002Ghost_in_the_minishell$ \001\033[0m\002");
+// 		if (!line)
+// 			break ;
+// 		if (*line)
+// 			add_history(line);
+// 		handle_line(line);
+// 		free(line);
+// 	}
+// 	printf("exit\n");
+// }
+
+// // ancien main d'aurelia
+// void	ft_run_tests(char **envp)
+// {
+// 	t_cmd	cmd;
+
+// 	cmd.argv = ft_split("/bin/ls -l", ' ');
+// 	cmd.infile = NULL;
+// 	cmd.outfile = NULL;
+// 	cmd.append = 0;
+// 	cmd.next = NULL;
+// 	execute_cmd(&cmd, envp);
+// 	free_array(cmd.argv);
+// 	cmd.argv = ft_split("/bin/echo Hello world", ' ');
+// 	execute_cmd(&cmd, envp);
+// 	free_array(cmd.argv);
+// }
---
+++ minishell/srcs/redirect.c
@@ -0,0 +1,100 @@
+#include "../includes/minishell.h"
+
+
+//execution des commandes (fork, execve, redirections, pipes, dup2, close)
+
+//overwrite > écrase le fichier s'il n'existe pas.
+//ouvre un fichier pour rediriger la sortie.
+void	exec_output_redirection(t_cmd *cmd)
+{
+	int	fd;
+
+	fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		puppetmaster_perror("open outfile");
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+		puppetmaster_perror("dup2");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+}
+
+// input < lit dans un fichier existant et remplace l'entrée standard (stdin)
+void	exec_input_redirection(t_cmd *cmd)
+{
+	int	fd;
+
+	fd = open(cmd->infile, O_RDONLY);
+	if (fd == -1)
+	{
+		puppetmaster_perror("open infile");
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(fd, STDIN_FILENO) == -1)
+	{
+		puppetmaster_perror("dup2");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+}
+
+// append >> ajoute à la fin d’un fichier (ou le crée si absent)
+void	exec_append_redirection(t_cmd *cmd)
+{
+	int	fd;
+
+	fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd == -1)
+	{
+		puppetmaster_perror("open outfile (append)");
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+		puppetmaster_perror("dup2");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+}
+
+// heredoc << simule un fichier temporaire contenant l’entrée jusqu’à un délimiteur
+void	exec_heredoc(t_cmd *cmd)
+{
+	int		fd;
+	char	*line;
+	char	*delimiter = cmd->infile; // contient le mot-clé de fin, comme "EOF"
+
+	fd = open("/tmp/.heredoc_tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		puppetmaster_perror("heredoc tmp file");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || ft_strcmp(line, delimiter) == 0)
+			break;
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+	}
+	free(line);
+	close(fd);
+
+	fd = open("/tmp/.heredoc_tmp", O_RDONLY);
+	if (fd == -1 || dup2(fd, STDIN_FILENO) == -1)
+	{
+		puppetmaster_perror("heredoc input");
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+}
