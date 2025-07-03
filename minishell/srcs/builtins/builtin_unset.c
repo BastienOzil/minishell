ac1d364402b1a8cbb9903ca50a0f1c33b9bf6b72
relia31 <george.aurelia.contact@gmail.com>
2025-07-03 21:06:57 +0200

    tout les builtins sont prets

--- minishell/srcs/builtins/builtin_unset.c
+++ minishell/srcs/builtins/builtin_unset.c
@@ -1,39 +1,44 @@
 #include "../../includes/minishell.h"
 
+
 //remove new variable to env 
 void	remove_var(char ***envp, char *var)
 {
-	int		i;
+	int		i = 0;
+	int		j = 0;
 	char	**new_env;
 
-	i = 0;
 	while ((*envp)[i])
 		i++;
 
-	new_env = malloc(sizeof(char *) * (i + 1));
+	new_env = malloc(sizeof(char *) * i);
 	if (!new_env)
 		return ;
 
 	i = 0;
 	while ((*envp)[i])
 	{
-		if ((*envp)[i] != var)
-		new_env[i] = ft_strdup((*envp)[i]);
+		if (!(ft_strncmp((*envp)[i], var, ft_strlen(var)) == 0
+			&& (*envp)[i][ft_strlen(var)] == '='))
+		{
+			new_env[j] = ft_strdup((*envp)[i]);
+			j++;
+		}
 		i++;
 	}
-	new_env[i] = ft_strdup(var);
-	new_env[i + 1] = NULL;
+	new_env[j] = NULL;
+
 	free_envp(envp);
 	*envp = new_env;
 }
 
+
 int	unset_builtin(char **args, char ***envp)
 {
 	char *var;
-
 	if (!args[1])
 		return (export_empty(envp));
-	var = is_arg_export(args[1]);
+	var = ft_strdup(args[1]);
 	if (!var)
 		return (1);		
     if (is_var_exist(var, envp))
