9fc123f3cbf92e44efbd703dff07312e65bc9c61
BastienOzil <bastien.ozil17@gmail.com>
2025-07-30 22:14:03 +0200

    norm

--- minishell/srcs/parse/expansion2.c
+++ minishell/srcs/parse/expansion2.c
@@ -6,15 +6,15 @@
 /*   By: bozil <bozil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:05:04 by bozil             #+#    #+#             */
-/*   Updated: 2025/07/30 17:05:11 by bozil            ###   ########.fr       */
+/*   Updated: 2025/07/30 19:52:40 by bozil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static char	*join_and_free(char *old_str, char *to_add)
+static char *join_and_free(char *old_str, char *to_add)
 {
-	char	*new_str;
+	char *new_str;
 
 	if (!old_str || !to_add)
 		return (NULL);
@@ -24,10 +24,10 @@ static char	*join_and_free(char *old_str, char *to_add)
 	return (new_str);
 }
 
-static char	*process_variable(char *str, int *i, char *result)
+static char *process_variable(char *str, int *i, char *result)
 {
-	char	*temp;
-	int		local_i;
+	char *temp;
+	int local_i;
 
 	local_i = 0;
 	temp = handle_var_expansion(&str[*i], &local_i);
@@ -41,9 +41,9 @@ static char	*process_variable(char *str, int *i, char *result)
 	return (result);
 }
 
-static char	*process_character(char *str, int *i, char *result)
+static char *process_character(char *str, int *i, char *result)
 {
-	char	*temp;
+	char *temp;
 
 	temp = ft_substr(str, *i, 1);
 	if (!temp)
@@ -56,10 +56,15 @@ static char	*process_character(char *str, int *i, char *result)
 	return (result);
 }
 
-char	*expand_string(char *str)
+static int is_expandable_char(char c)
 {
-	char	*result;
-	int		i;
+	return (ft_isalnum(c) || c == '_' || c == '?');
+}
+
+char *expand_string(char *str)
+{
+	char *result;
+	int i;
 
 	if (!str)
 		return (NULL);
@@ -69,8 +74,7 @@ char	*expand_string(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '$' && str[i + 1] && (ft_isalnum(str[i + 1]) || str[i
-				+ 1] == '_' || str[i + 1] == '?'))
+		if (str[i] == '$' && str[i + 1] && is_expandable_char(str[i + 1]))
 			result = process_variable(str, &i, result);
 		else
 			result = process_character(str, &i, result);
