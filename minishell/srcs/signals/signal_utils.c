99aed7d9abb32f6fb5a3bfd1239d9a537d50ef64
Bastien <bozil@student.42nice.fr>
2025-07-30 17:10:08 +0200

    signqls too

--- minishell/srcs/signals/signal_utils.c
+++ minishell/srcs/signals/signal_utils.c
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   signal_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurelia <aurelia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bozil <bozil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:12:41 by bozil             #+#    #+#             */
-/*   Updated: 2025/07/26 10:14:23 by aurelia          ###   ########.fr       */
+/*   Updated: 2025/07/30 15:11:18 by bozil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// Gère le statut de sortie en fonction du signal reçu
 void	handle_signal_exit_status(int sig)
 {
 	if (sig == SIGINT)
@@ -21,27 +20,25 @@ void	handle_signal_exit_status(int sig)
 		g_exit_status = SIGQUIT_CODE;
 }
 
-// Convertit un signal en code de sortie
 int	signal_to_exit_code(int sig)
 {
-	switch (sig)
+	if (sig)
 	{
-	case SIGINT:
-		return (SIGINT_CODE);
-	case SIGQUIT:
-		return (SIGQUIT_CODE);
-	default:
-		return (sig + 128); // 128 pour la convention Unix
+		if (sig == SIGINT)
+			return (SIGINT_CODE);
+		else if (sig == SIGQUIT)
+			return (SIGQUIT_CODE);
+		else
+			return (sig + 128);
 	}
+	return (0);
 }
 
-// Remet à zéro l'état des signaux
 void	reset_signal_state(void)
 {
 	g_exit_status = 0;
 }
 
-// Vérifie si un signal a été reçu
 int	signal_pending(void)
 {
 	return (g_exit_status != 0);
--- minishell/srcs/signals/signal_utils2.c
+++ minishell/srcs/signals/signal_utils2.c
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   signal_utils2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurelia <aurelia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bozil <bozil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:13:16 by bozil             #+#    #+#             */
-/*   Updated: 2025/07/26 10:12:27 by aurelia          ###   ########.fr       */
+/*   Updated: 2025/07/30 15:04:26 by bozil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// Attend qu'un processus enfant se termine et gère les signaux
 int	wait_for_child(pid_t pid)
 {
 	int	status;
@@ -22,17 +21,13 @@ int	wait_for_child(pid_t pid)
 	result = waitpid(pid, &status, 0);
 	if (result == -1)
 		return (-1);
-	// Si le processus s'est terminé normalement
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
-	// Si le processus s'est terminé par un signal
 	if (WIFSIGNALED(status))
 	{
 		sig = WTERMSIG(status);
-		// Afficher newline pour SIGINT seulement
 		if (sig == SIGINT)
 			write(STDOUT_FILENO, "\n", 1);
-		// Afficher message pour SIGQUIT
 		else if (sig == SIGQUIT)
 			write(STDOUT_FILENO, "Quit (core dumped)\n", 19);
 		return (signal_to_exit_code(sig));
@@ -40,7 +35,6 @@ int	wait_for_child(pid_t pid)
 	return (EXIT_FAILURE);
 }
 
-// Vérifie si le processus actuel doit être interrompu
 int	should_interrupt(void)
 {
 	return (g_exit_status == SIGINT_CODE);
